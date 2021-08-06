da1462d9866ad0f8dc8fce9347eabb62fa73bc14
PaddyHegarty <paddyhegs@gmail.com>
2021-08-06 15:54:53 +0100

    fixed key issue

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const ArticleItem = ({ article }) => {
   return (
     <Link href="/article/[id]" as={`/article/${article.id}`} >
-      <a className={articleStyles.card}>
+      <a className={articleStyles.card} key={article.id}>
         <h3>{article.title} &rarr;</h3>
         <p>{article.body}</p>
       </a>

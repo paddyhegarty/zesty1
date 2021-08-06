d6885b59d8d81598b9a60ec4f3c72eacde148d12
PaddyHegarty <paddyhegs@gmail.com>
2021-08-06 15:15:34 +0100

    first commit

---
+++ components/ArticleList.js
@@ -0,0 +1,16 @@
+import articleStyles from '../styles/Article.module.css'
+import ArticleItem from './ArticleItem'
+
+const ArticleList = ({ articles }) => {
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article) => {
+        return (
+          <ArticleItem article={article} />
+        )
+      })}
+    </div>
+  )
+}
+
+export default ArticleList

a075ff00b4b22f74cb9722512a356bfeacde52f3
PaddyHegarty <paddyhegs@gmail.com>
2021-08-15 15:13:54 +0100

    removed tabs from about us

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import React from 'react';
-import Tabs from "../components/tabs/Tabs";
 
 const about = () => {
   return (
@@ -8,19 +7,6 @@ const about = () => {
       <Head>
         <title>Abouts Us</title>
       </Head>
-      <h1>About Us</h1>
-      <h1>Tabs Demo</h1>
-      <Tabs>
-        <div label="Gator">
-          See ya later, <em>Alligator</em>!
-        </div>
-        <div label="Croc">
-          After 'while, <em>Crocodile</em>!
-        </div>
-        <div label="Sarcosuchus">
-          Nothing to see here, this tab is <em>extinct</em>!
-        </div>
-      </Tabs>
     </div>
   )
 }
